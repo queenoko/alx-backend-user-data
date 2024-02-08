f409209f04f2787fe7bd197485874ea598829f05
queenoko <quimaudongwo@gmail.com>
2024-02-08 15:07:39 +0100

    is valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,10 @@ def hash_password(password: str) -> bytes:
     using a random salt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """This function Checks if a hashed password
+    was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
